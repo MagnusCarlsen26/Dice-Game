cb18289213235d303cbba3213f7668a4ccd54892
MagnusCarlsen26 <khushal.sindhav26@gmail.com>
2023-08-04 18:27:36 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Board from './components/Board'
+import Rules from './components/Rules'
+function App() {
+  return (
+    <div className='app'>
+        <Board />
+        <Rules />
+    </div>
+  );
+}
+
+export default App;
